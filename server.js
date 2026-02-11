73e61cd812de531f18e39ba72ad5af694ddf3c75
Suchandra Mondal <suchandramondal@Administrators-MacBook-Pro.local>
2026-02-11 15:58:35 +0530

    new changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Azure 🚀');
+  res.send('Hello this is CI/CD pupelines 🚀');
 });
 
 app.listen(port, () => {
